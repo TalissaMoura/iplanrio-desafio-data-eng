bb902e1552c143e5a7071087449359cd0ad3cad5
TalissaMoura <talissa.ayres@gmail.com>
2026-03-02 12:46:18 -0300

    update: apply lint

--- dw/models/mart/app_terceirizados.sql
+++ dw/models/mart/app_terceirizados.sql
@@ -16,19 +16,23 @@ ultima_particao_id as (
 orgaos_superiores_terceirizado as (
     select
         fct_contratos.id_terceirizado,
-        org_sup.unidade_gestora_nome
+        dim_terc.cnpj,
+        dim_terc.cpf,
+        dim_orgaos_superiores.orgao_superior_sigla
     from {{ ref('fact_contratos_terceirizados') }} as fct_contratos
     inner join ultima_particao_id as tempo
         on fct_contratos.id_tempo = tempo.ultima_particao_id
-    left join {{ ref('dim_orgaos_superiores') }} as org_sup
-        on fct_contratos.id_orgao_superior = org_sup.id_orgao_superior
+    left join {{ ref('dim_terceirizados') }} as dim_terc
+        on fct_contratos.id_terceirizado = dim_terc.id_terceirizado
+    left join {{ ref('dim_orgaos_superiores') }} as dim_orgaos_superiores
+        on
+            fct_contratos.id_orgao_superior
+            = dim_orgaos_superiores.id_orgao_superior
 )
 
 select
-    dim_terc.id_terc,
-    org_sup.unidade_gestora_nome,
-    dim_terc.cnpj,
-    dim_terc.cpf
-from {{ ref('dim_terceirizados') }} as dim_terc
-left join orgaos_superiores_terceirizado as org_sup
-    on dim_terc.id_terc = org_sup.id_terceirizado
+    id_terceirizado,
+    cnpj,
+    cpf,
+    orgao_superior_sigla
+from orgaos_superiores_terceirizado
