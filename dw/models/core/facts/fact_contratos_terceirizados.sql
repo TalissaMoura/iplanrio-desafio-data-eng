a8d2300500ff0ecc5a00fe1b27656aef52d32bd3
TalissaMoura <talissa.ayres@gmail.com>
2026-03-02 12:45:24 -0300

    update: apply lint to fact

--- dw/models/core/facts/fact_contratos_terceirizados.sql
+++ dw/models/core/facts/fact_contratos_terceirizados.sql
@@ -4,23 +4,21 @@ config(
     schema='prata',
     tags=['core', 'fact'],
     incremental_strategy='merge',
-    unique_key=['id_fato'] 
+    unique_key=['id_fato']
 )
 }}
 
 with source as (
     select
-    
+
         id_terceirizado,
         (ano * 100 + mes_numero)::int as id_tempo,
         salario_mensal_valor,
         custo_mensal_valor,
         data_processamento,
 
-
         hash(numero_contrato) as id_contrato,
         hash(orgao_nome, orgao_sigla) as id_orgao,
-
 
         hash(unidade_gestora_nome, unidade_gestora_codigo) as id_orgao_superior
 
@@ -36,5 +34,5 @@ from source as s
     -- No modo incremental, pegamos apenas dados novos para processar
     where
         (s.ano * 100 + s.mes_numero)
-        >= (select max(ano * 100 + mes_numero) from {{ this }})
+        >= (select max(t.ano * 100 + t.mes_numero) from {{ this }} as t)
 {% endif %}
