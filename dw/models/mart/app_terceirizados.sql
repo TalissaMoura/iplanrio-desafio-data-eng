d3370100c72f625ea48c7ad9de1ba5ce84ea5f32
TalissaMoura <talissa.ayres@gmail.com>
2026-03-02 17:51:43 -0300

    update: update join type

--- dw/models/mart/app_terceirizados.sql
+++ dw/models/mart/app_terceirizados.sql
@@ -22,9 +22,9 @@ orgaos_superiores_terceirizado as (
     from {{ ref('fact_contratos_terceirizados') }} as fct_contratos
     inner join ultima_particao_id as tempo
         on fct_contratos.id_tempo = tempo.ultima_particao_id
-    left join {{ ref('dim_terceirizados') }} as dim_terc
+    inner join {{ ref('dim_terceirizados') }} as dim_terc
         on fct_contratos.id_terceirizado = dim_terc.id_terceirizado
-    left join {{ ref('dim_orgaos_superiores') }} as dim_orgaos_superiores
+    inner join {{ ref('dim_orgaos_superiores') }} as dim_orgaos_superiores
         on
             fct_contratos.id_orgao_superior
             = dim_orgaos_superiores.id_orgao_superior
